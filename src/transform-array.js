d667d99c6e756a3c682b766b2ef0786acba23b4e
Masha <77979222+sobolevamasha@users.noreply.github.com>
2021-03-13 14:29:26 +0300

    feat: add solutions

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(arr) {
-  
-};
+module.exports = function transform(array) {
+    if (Array.isArray(array)) {
+    let newArray = [].concat(array);
+
+    let answer = newArray.map((item, index, array) => {
+        if (typeof item === 'string') {
+        if (item === '--double-next') {
+            array[index + 1] !== undefined ? item = array[index + 1] : item = '';
+        } else if (item === '--double-prev') {
+            array[index - 1] !== undefined ? item = array[index - 1] : item = '';
+        } else if (item === '--discard-next') {
+            array[index + 1] !== undefined ? (item = '', array[index + 1] = '') : item = '';
+        } else if (item === '--discard-prev') {
+            array[index - 1] !== undefined ? (item = 'del-prev') : item = '';
+        }
+    } return item;
+    });
+
+    for (let i = 0; i < answer.length; i++) {
+        let indexDel = answer.indexOf('del-prev') - 1;
+        indexDel > 0 ? answer.splice(indexDel, 2) : answer;
+    }
+
+    return answer.filter((item) => item !== '');
+}
+
+throw Error("Error")
+
+}
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,67 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(meme) {
+    this.meme = meme;
+  }
+  vigenerSquare() {
+    const words = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+    const square = [];
+    for (var i = 0; i < words.length; i++) {
+      square[i] = words.slice(i).concat(words.slice(0, i));
+    }
+    return square;
+  }
+  encrypt(string, key) {
+    if (string && key) {
+      const square = this.vigenerSquare();
+      const words = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+      let newString = string.toUpperCase();
+      let newKey = key.split('')
+      let i = newKey.length;
+      for (i = i; i < string.length; i += i) {
+        key = key.concat(key);
+      }
+      newKey = key.toUpperCase().split('').slice(0, string.length)
+      let str = "";
+      let j = 0
+      for (let i = 0; i < newString.length; i++) {
+        if (words.indexOf(newString[i]) !== -1) {
+          str += square[words.indexOf(newString[i])][words.indexOf(newKey[j])];
+          j++
+        } else str += newString[i];
+      }
+      if (this.meme !== false) {
+        return str;
+      } else return str.split('').reverse().join('');
+    } else throw Error('Error')
+  }
+
+  decrypt(string, key) {
+    if (string && key) {
+      const square = this.vigenerSquare();
+      const words = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+      let newString = string.toUpperCase();
+      let newKey = key.split('');
+      let i = newKey.length;
+      for (i = i; i < string.length; i += i) {
+        key = key.concat(key);
+      }
+      newKey = key.toUpperCase().split('').slice(0, string.length)
+      let str = "";
+      let j = 0
+      for (let i = 0; i < newString.length; i++) {
+        if (words.indexOf(newString[i]) !== -1) {
+          let row = words.indexOf(newKey[j])
+          let coll = square[row].indexOf(newString[i]);
+          str += words[coll];
+          j++
+        } else str += newString[i];
+      }
+      if (this.meme !== false) {
+        return str;
+      } else return str.split('').reverse().join('');
+    } else throw Error('Error')
   }
 }
-
 module.exports = VigenereCipheringMachine;
