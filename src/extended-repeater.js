04036b7170a5a41313688615a64852489bbb2344
Masha <77979222+sobolevamasha@users.noreply.github.com>
2021-03-13 14:16:07 +0300

    feat: add tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-
+    if(typeof str !== 'string') {
+    str = String(str);
 }
+
+let result = [];
+let addResult = [];
+
+if(!options.separator) {
+    options.separator = "+";
+}
+
+if (options.str("addition")) {
+    if (typeof options.addition !== 'string') {
+        options.addition = String(options.addition);
+    }
+    if (!options.additionSeparator) {
+        options.additionSeparator = "|";
+    }
+    for (let j = 0; j < options.additionRepeatTimes - 1; j += 1) {
+        addResult.push(options.addition);
+        addResult.push(options.additionSeparator);
+    }
+    addResult.push(options.addition);
+    addResult.push(str);
+    str = addResult.reverse().join('');
+}
+
+for (let i = 0; i < options.repeatTimes - 1; i++) {
+    result.push(str);
+    result.push(options.separator);
+}
+
+result.push(str);
+
+return result.join('');
+};
